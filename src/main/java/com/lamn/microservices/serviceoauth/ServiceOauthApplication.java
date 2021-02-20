a599534a24ed453e7a28fbd2b80b25bbecf0cd7a
Luis Navarro <navarrojr89@gmail.com>
2021-02-20 12:50:07 +0100

    Sets OAuth Authorization

--- src/main/java/com/lamn/microservices/serviceoauth/ServiceOauthApplication.java
+++ src/main/java/com/lamn/microservices/serviceoauth/ServiceOauthApplication.java
@@ -1,9 +1,12 @@
 package com.lamn.microservices.serviceoauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * The type Service oauth application.
@@ -11,7 +14,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-public class ServiceOauthApplication {
+public class ServiceOauthApplication implements CommandLineRunner {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /**
      * The entry point of application.
@@ -22,4 +28,13 @@ public class ServiceOauthApplication {
         SpringApplication.run(ServiceOauthApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        String password = "12345";
+
+        for (int i = 0; i < 4; i++) {
+            String passwordBCrypt = bCryptPasswordEncoder.encode(password);
+            System.out.println(passwordBCrypt);
+        }
+    }
 }
---
+++ src/main/java/com/lamn/microservices/serviceoauth/security/SpringSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.lamn.microservices.serviceoauth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * The type Spring security config.
+ */
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    /**
+     * Password encoder b crypt password encoder.
+     *
+     * @return the b crypt password encoder
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
